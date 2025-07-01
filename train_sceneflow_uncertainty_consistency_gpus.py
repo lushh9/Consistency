6028af33a9f07bde69c71b19dfba65e6195023c0
lushh9 <71750557+lushh9@users.noreply.github.com>
2025-07-01 12:52:46 +0800

    Update train_sceneflow_uncertainty_consistency_gpus.py

--- train_sceneflow_uncertainty_consistency_gpus.py
+++ train_sceneflow_uncertainty_consistency_gpus.py
@@ -343,9 +343,15 @@ def constant_for_dataset(model, train_loader, epoch, path, threshold, cfg, accel
                 mask_invalid = 0
                 with lock2:
                     with h5py.File(out_path, 'a') as f2:
-                        print("keys长度:", len(list(f2.keys())))
+                        dataset_keys = []
+                        def collect_datasets(name, obj):
+                            if isinstance(obj, h5py.Dataset):
+                                dataset_keys.append(name)
+                        f2.visititems(collect_datasets)
                         print("Keys列表:")
-                        print(list(f2.keys()))
+                        for k in dataset_keys:
+                            print(k)
+                        print("keys长度:", len(dataset_keys))
                         print('地址：', key)
                         if key in f2:
                             f2_temp = f2[sample["img1_path"][k]][()]
